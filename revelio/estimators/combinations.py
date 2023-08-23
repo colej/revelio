d28ccb095ff39b393d205b420071b07b0d1ca370
colej <johnston.ccj@gmail.com>
2023-08-23 16:57:24 +0200

    changed weighting functions for estimators

--- revelio/estimators/combinations.py
+++ revelio/estimators/combinations.py
@@ -9,7 +9,7 @@ from data taken in multiple filters into a single, weighted
 frequency of maximum variability estimate.
 """
 
-def get_weights_simple(N_filters):
+def get_weights_equal(N_filters):
     """
     @input (int): N_filters --> total number of filters to be combined
 
@@ -28,7 +28,28 @@ def get_weights_simple(N_filters):
     return np.array([1./N_filters for ii in range(N_filters)])
 
 
-def get_weights_per_filter(variances):
+def get_weights_n_obs(n_obs):
+    """
+    @input (array): N_filters --> array containing number of observations per
+                                  filter.
+
+    ----------------------------------------------------------------------------
+    ----------------------------------------------------------------------------
+
+    @output (array): weights --> weight per filter
+
+    ----------------------------------------------------------------------------
+    ----------------------------------------------------------------------------
+
+    This function returns weights according to the total number of data npoints
+    obtained in that filter; i.e. the weight for a filter f is: w_f = n_obs_f/n_obs_total
+    """
+    total_obs = np.sum(n_obs)
+
+    return np.array([n_obs[ii]/total_obs for ii in range(len(n_obs))])
+
+
+def get_weights_variance(variances):
 
     """
     @input (float): variance --> variance on the frequency estimate for a given
@@ -46,14 +67,16 @@ def get_weights_per_filter(variances):
     such that the weight for a given filter f is: w_f = 1/(sigma_f)^2
     """
 
-    return 1./variance
+    return 1./variances
 
 
-def get_combined_frequency(frequencies, variances, simple_weights=False):
+def get_combined_frequency(frequencies, variances, n_obs=None, type_weights='equal'):
     """
     @input (array): frequencies --> frequency estimates per filter
     @input (array): variances --> variance estimates per filter
-
+    @input (list/array): n_obs --> number of observations per filter. Only to be
+                                  used if type_weights == 'nobs'
+    @input (str): type_weights --> the method for determining the weights per filter
     ----------------------------------------------------------------------------
     ----------------------------------------------------------------------------
 
@@ -70,14 +93,18 @@ def get_combined_frequency(frequencies, variances, simple_weights=False):
     """
 
     ## Determine the weights
-    ## If we want to give equal weight to each filter, we use "simple_weights"
-    if simple_weights:
-        weights = get_weights_simple(len(frequencies))
-
-    ## Otherwise, we set the weight of a filter according to the variance of the
+    ## We can set the weight of a filter according to the variance of the
     ## frequency estimated from the Monte Carlo iterations.
+    if type_weights=='variance':
+        weights = get_weights_variance(variances)
+
+    ## Or we can give wights according to the number of observations per filter
+    elif type_weights=='n_obs':
+        weights = get_weights_n_obs(n_obs)
+
+    ## Otherwise, we can give equal weight to each filter, we use "equal"
     else:
-        weights = get_weights_per_filter(variances)
+        weights = get_weights_equal(len(frequencies))
 
     ## we calculate the combined frequency according to:
     ## nu_hat = [ sum_f->F ( weights_f * frequencies_f) ] / [ sum_f->F ( weights_f ) ]
